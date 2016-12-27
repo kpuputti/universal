171a4da7ed854940f6426795df07202574376bc3
Kimmo Puputti <git@kimmo.kpuputti.fi>
2016-12-27 15:19:51 +0200

    Implement a simple app with routing, redirect, etc.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Link, Redirect, BrowserRouter, Match, Miss } from 'react-router';
 
-class App extends Component {
-  render() {
+const ITEMS = ['a', 'b', 'c'];
+
+function NotFound() {
+  return (
+    <div>
+      <p>Page not found.</p>
+      <Link to="/">back to home</Link>
+    </div>
+  );
+}
+
+function OldIndex() {
+  return <Redirect to="/"/>;
+}
+
+function Item(props) {
+  const id = props.params.id;
+  if (!ITEMS.includes(id)) {
+    return <NotFound/>;
+  }
+  return (
+    <div>
+      <p>Item {`${id}`}</p>
+      <p><Link to="/">back to home</Link></p>
+      <p><Link to="/home">old home</Link></p>
+    </div>
+  );
+}
+
+function Index(props) {
+
+  function itemLink(i) {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <li key={i}>
+        <Link to={`/items/${i}`}>{`Item ${i}`}</Link>
+      </li>
     );
   }
+
+  return (
+    <div>
+      <p>Home page</p>
+      <ul>
+        {ITEMS.map(itemLink)}
+        {itemLink('notfound')}
+      </ul>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={Index} />
+        <Match exactly pattern="/home" component={OldIndex} />
+        <Match pattern="/items/:id" component={Item} />
+        <Miss component={NotFound}/>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
